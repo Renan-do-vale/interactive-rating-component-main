b2a5eb045b54c6cc81bfd70606f86a8bd396e409
Renan do vale ferreira <Renandovaleferreira@gmail.com>
2023-07-09 21:34:03 -0300

    v3semifinal

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-const primary = document.getElementsByClassName('primary')
 const secundary = document.querySelector(".secundary")
 
-function minhaFuncao() {
-    console.log('ola')
-    primary.style.display = 'none'
+const numbers = document.querySelectorAll(".check-number span")
+
+let rate = null
+
+
+function mark(event) {
+    const ativo = document.querySelector(".ativo")
+    if(ativo){
+        event.classList.remove("ativo")
+        rate = null
+    }else {
+        event.classList.add("ativo")   
+        rate = event.innerText
+    }
 }
 
-document.getElementById("button").onclick = function() {
-    minhaFuncao();
-  };
+
+
+
+function minhaFuncao() {
+    const primary = document.querySelector('.primary')
+    const secundary = document.querySelector(".secundary")
+    const chosen = document.querySelector(".chosen")
+    if(rate) {
+        primary.style.display = 'none'
+        secundary.style.display = 'flex'
+    }
+    chosen.innerText = rate
+    console.log(rate)
+
+}
